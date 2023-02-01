5dc07612a51b0e6f28fc197b1233a6b77be195db
Denis RN <denys.marshavka@swansoftwaresolutions.com>
2023-02-01 20:36:30 +0200

    Added new 'Failure messages' options, of the 'search_bloc' file

--- lib/feature/presentation/bloc/search_bloc/search_bloc.dart
+++ lib/feature/presentation/bloc/search_bloc/search_bloc.dart
@@ -4,6 +4,9 @@ import 'package:rick_and_morty_app/feature/domain/usecases/search_person.dart';
 import 'package:rick_and_morty_app/feature/presentation/bloc/search_bloc/search_event.dart';
 import 'package:rick_and_morty_app/feature/presentation/bloc/search_bloc/search_state.dart';
 
+const SERVER_FAILURE_MESSAGE = 'Server Failure';
+const CACHED_FAILURE_MESSAGE = 'Cache Failure';
+
 class PersonSearchBloc extends Bloc<PersonSearchEvent, PersonSearchState> {
   final SearchPerson searchPerson;
 
@@ -30,9 +33,9 @@ class PersonSearchBloc extends Bloc<PersonSearchEvent, PersonSearchState> {
   String _mapFailureToMessage(Failure failure) {
     switch (failure.runtimeType) {
       case ServerFailure:
-        return 'Server Failure';
+        return SERVER_FAILURE_MESSAGE;
       case CacheFailure:
-        return 'Cache Failure';
+        return CACHED_FAILURE_MESSAGE;
       default:
         return 'Unexpected Error';
     }
