4feb8cdebae2581d3c2320a9721f4cfcfd535750
Denis RN <denys.marshavka@swansoftwaresolutions.com>
2023-02-07 20:06:35 +0200

    Fixed the 'PersonLocalDataSource' Data file

--- lib/feature/data/datasources/person_local_data_source.dart
+++ lib/feature/data/datasources/person_local_data_source.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:rick_and_morty_app/core/error/exception.dart';
 import 'package:rick_and_morty_app/feature/data/models/pagination_list_info_model.dart';
 import 'package:rick_and_morty_app/feature/data/models/person_model.dart';
+import 'package:rick_and_morty_app/feature/data/models/persons_list_data_with_pagination_info_model.dart';
 import 'package:rick_and_morty_app/feature/domain/entities/persons_list_data_with_pagination_info_entity.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 const CACHED_PERSONS_LIST_KEY = 'CACHED_PERSONS_LIST';
 const CACHED_PERSONS_LIST_INFO_KEY = 'CACHED_PERSONS_LIST_INFO';
@@ -27,15 +29,14 @@ class PersonLocalDataSourceImpl implements PersonLocalDataSource {
         sharedPreferences.getStringList(CACHED_PERSONS_LIST_KEY);
     final jsonPersonsListInfo =
         sharedPreferences.getString(CACHED_PERSONS_LIST_INFO_KEY);
-    late PersonsListDataWithPaginationInfoEntity result = {
-      'data': [],
-      'info': {
-        'count': 0,
-        'pages': 0,
-        'next': null,
-        'prev': null,
-      }
-    } as PersonsListDataWithPaginationInfoEntity;
+    late PersonsListDataWithPaginationInfoEntity result =
+        PersonsListDataWithPaginationInfoModel(
+      data: const [],
+      info: const PaginationListInfoModel(
+        count: 0,
+        pages: 0,
+      ),
+    );
 
     if (jsonPersonsList?.isNotEmpty == true) {
       result.data = jsonPersonsList!
